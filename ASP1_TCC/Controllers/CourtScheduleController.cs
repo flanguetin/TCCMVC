63331d0eb09b577d0f084c65aff2e8c2bba3f1a7
MisterX25 <Xavier.Carrel@cpnv.ch>
2015-11-25 18:08:04 +0100

    wip

---
+++ ASP1_TCC/Controllers/CourtScheduleController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ASP1_TCC.ViewModels;
+
+namespace ASP1_TCC.Controllers
+{
+    public class CourtScheduleController : Controller
+    {
+        private Entities db = new Entities();
+
+        // GET: CourtSchedule
+        public ActionResult Index(int courtid)
+        {
+            courts theCourt = db.courts.Find(courtid);
+            theCourt.reservations.OrderBy(reservations => reservations.date);
+
+            CourtScheduleViewModel vm = new CourtScheduleViewModel();
+            vm.courtId = courtid;
+            vm.courtName = theCourt.name;
+            vm.from = DateTime.Now;
+            vm.hourStart = 7;
+            vm.hourEnd = 20;
+            vm.dateSpan = 7;
+            vm.selectedBookings = new List<reservations>();
+            DateTime limit = DateTime.Now.AddDays(vm.dateSpan);
+            foreach (reservations r in theCourt.reservations)
+                if ((r.date > DateTime.Now) && (r.date < limit))
+                    vm.selectedBookings.Add(r);
+
+            return View(vm);
+        }
+    }
+}
---
+++ ASP1_TCC/ViewModels/CourtScheduleViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ASP1_TCC.ViewModels
+{
+    public class CourtScheduleViewModel
+    {
+        public int courtId;
+        public string courtName;
+        public List<reservations> selectedBookings;
+        public DateTime from; // date from which we show
+        public int dateSpan; // Number of days to show
+        public int hourStart; // hour of the first booking to show for each day
+        public int hourEnd; // hour of the start of the last booking of the day
+    }
+}
